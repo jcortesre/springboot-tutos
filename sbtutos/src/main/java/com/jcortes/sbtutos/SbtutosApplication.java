f97337a2f712aa8147ecb81fffe529f3ed8bdae1
Juan Carlos Cortes <juancortesre@gmail.com>
2018-01-23 16:07:21 -0500

    add new spring-boot project for udemy tutorial

---
+++ sbtutos/src/main/java/com/jcortes/sbtutos/SbtutosApplication.java
@@ -0,0 +1,12 @@
+package com.jcortes.sbtutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtutosApplication.class, args);
+	}
+}
