bb90262a3e42d3f18fd605041481c064e6acf736
Juan Carlos Cortes <juancortesre@gmail.com>
2018-01-24 21:43:00 -0500

    add Working with static content and webjars

--- toysforshots/src/main/java/com/toysforshots/controller/PageController.java
+++ toysforshots/src/main/java/com/toysforshots/controller/PageController.java
@@ -1,13 +1,5 @@
 package com.toysforshots.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class PageController {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello world";
-	}
 }
